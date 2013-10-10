5aa47f22da4ced6e7d23718bc5ca7c465cb7f3be
Owen Garland <Garland.owen@gmail.com>
2013-10-10 16:22:40 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+##Website Checker
+
+If a website is down and you want to be notified as soon as it comes back up then this script makes that a whole lot easier!
+
+Simply go to http://isup.me/www.websitethatsdown.com and isup.me will test if the site is down or not, my script will keep refreshing that page until the website is back up.
+
+When the site comes back online it will redirect you to the website.
+
+Enjoy!
---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name           Website Checker
+// @namespace      bag-man
+// @description    Checks if a website is up or not via isup.me
+// @include        http://www.isup.me/*
+// @include	   http://isup.me/*
+// @grant	   none
+// ==/UserScript==
+
+var url = document.getElementsByClassName("domain")[0].innerHTML;
+if(/\bup\b/.test(document.getElementById("container").innerHTML)) {
+//  alert("The site is back up. Press OK to continue.");
+  window.location.href = url;
+}
+setTimeout(function() { document.location.reload(); } ,4000);
